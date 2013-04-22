3ae971f1e8a7322df005600555d9d984ee5783a4
Jeff Cross <middlefloor@gmail.com>
2013-04-22 13:56:58 -0700

    Added swipe sample.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,6 +2,14 @@
 
 angular.module('ngswipeDemoApp')
   .controller('MainCtrl', function ($scope) {
+  	$scope.swipeLeft = function () {
+  		console.log('swiped left');
+  	};
+
+  	$scope.swipeRight = function () {
+  		console.log('swiped right');
+  	};
+  	
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
