83a63afeb451bb11627120364c2743f3c57ee56a
Braden Shepherdson <braden.shepherdson@gmail.com>
2013-04-23 16:53:33 -0700

    Working first pass of the demo app.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,25 @@
 
 angular.module('ngswipeDemoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.pages = [
+      {
+        id: 1,
+        color: 'red'
+      }, {
+        id: 2,
+        color: 'blue'
+      }, {
+        id: 3,
+        color: 'green'
+      }, {
+        id: 4,
+        color: 'yellow'
+      }, {
+        id: 5,
+        color: 'pink'
+      }, {
+        id: 6,
+        color: 'cyan'
+      }
     ];
   });
