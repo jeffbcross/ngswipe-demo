9bea0dd82f3d2dbdefff3e5192d9b5469d871c31
Jeff Cross <middlefloor@gmail.com>
2013-05-13 13:14:37 -0700

    Fixed bug with navigating to correct article detail.

--- app/scripts/controllers/ArticleDetail.js
+++ app/scripts/controllers/ArticleDetail.js
@@ -2,15 +2,13 @@
 
 angular.module('ngswipeDemoApp')
   .controller('ArticleDetailCtrl', ['$scope', '$rootScope', '$routeParams', '$http', '$sanitize', '$window', 'Articles', 'FeedManager', '$location', function ($scope, $rootScope, $routeParams, $http, $sanitize, $window, Articles, FeedManager, $location) {
-    $scope.bootstrap = function () {
-      var feed = FeedManager.getSelected();
-      $scope.articleIndex = $routeParams.index || 0;
-      $scope.detailFeed = Articles.fetch(feed.href);
-    };
-
+    
+    var feed = FeedManager.getSelected();
+    $scope.detailFeed = Articles.fetch(feed.href);
+    //TODO: Check index against actual position within feed, in case link is an outdated bookmark.
+    $scope.articleIndex = $routeParams.index || 0;
+    
     $scope.goBack = function () {
       $window.history.back();
     };
-    
-    $scope.bootstrap();
   }]);
