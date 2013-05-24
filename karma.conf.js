4435fd29f38d498027e294420593f9748d36ed56
Jeff Cross <middlefloor@gmail.com>
2013-05-24 10:03:48 -0700

    Updated karma conf to new syntax.

--- karma.conf.js
+++ karma.conf.js
@@ -1,68 +1,35 @@
-// Karma configuration
-
-// base path, that will be used to resolve files and exclude
-basePath = 'app/';
-
-// list of files / patterns to load in the browser
-files = [
-  'scripts/angular/angular.js',
-  'components/angular-mocks/angular-mocks.js',
-  'components/angular-mobile/*.js',
-  'components/angular-resource/*.js',
-  'components/angular-sanitize/*.js',
-  'scripts/*.js',
-  'scripts/**/*.js',
-  '../test/mock/*.js',
-  '../test/spec/**/*.js',
-  'views/**.html'
-];
-
-plugins = ['karma-ng-html2js-preprocessor', 'karma-jasmine', 'karma-chrome-launcher'];
-
-frameworks = [
-  'jasmine'
-];
-
-// list of files to exclude
-exclude = [];
-
-preprocessors = {
-  'views/**.html': 'html2js'
-};
-
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
-
-// web server port
-port = 8080;
-
-// cli runner port
-runnerPort = 9100;
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+module.exports = function (karma) {
+  karma.configure({
+    files: [
+      'app/scripts/angular/angular.js',
+      'app/components/angular-mocks/angular-mocks.js',
+      'app/components/angular-mobile/*.js',
+      'app/components/angular-resource/*.js',
+      'app/components/angular-sanitize/*.js',
+      'app/scripts/*.js',
+      'app/scripts/**/*.js',
+      'app/views/**.html',
+      'test/mock/*.js',
+      'test/spec/**/*.js'
+    ],
+    plugins: [
+      'karma-ng-html2js-preprocessor',
+      'karma-jasmine',
+      'karma-chrome-launcher'
+    ],
+    frameworks: [
+      'jasmine'
+    ],
+    preprocessors : {
+      'views/**.html': 'html2js'
+    },
+    reporters : ['progress'],
+    port : 8080,
+    runnerPort : 9100,
+    colors : true,
+    autoWatch : true,
+    browsers : ['Chrome'],
+    captureTimeout : 5000,
+    singleRun : false
+  });
+}
